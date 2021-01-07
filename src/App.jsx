6e6dffe9df1687fd32f976e02e171294f468e7dc
Mohamed Ammar <mohamed.ammar86@gmail.com>
2021-01-06 23:54:00 -0500

    initial styling to the search page

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import "./styles/main.scss";
 function Main() {
   return (
     <main>
-      <h3>Search Weather condition by city</h3>
-      <form>
-        <input type="text"></input>
-        <button type="submit"></button>
-      </form>
+      <div>
+        <h1>Search Weather condition by city</h1>
+        <form>
+          <input type="text"></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </main>
   );
 }
